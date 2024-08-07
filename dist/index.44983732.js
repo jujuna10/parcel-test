93c0bbc65f2c091cc7bce854a99208a2c1bdc891
jujuna10 <jujunaniko@gmail.com>
2024-08-07 22:35:13 +0400

    commit

---
+++ dist/index.44983732.js
@@ -0,0 +1,11 @@
+const p = document.getElementById("p");
+const plus = document.getElementById("plus");
+const minus = document.getElementById("minus");
+plus.addEventListener("click", ()=>{
+    p.textContent = Number(p.textContent) + 1;
+});
+minus.addEventListener("click", ()=>{
+    p.textContent = Number(p.textContent) - 1;
+});
+
+//# sourceMappingURL=index.44983732.js.map
---
+++ dist/index.9bffb773.js
@@ -0,0 +1,2 @@
+const t=document.getElementById("p"),e=document.getElementById("plus"),n=document.getElementById("minus");e.addEventListener("click",()=>{t.textContent=Number(t.textContent)+1}),n.addEventListener("click",()=>{t.textContent=Number(t.textContent)-1});
+//# sourceMappingURL=index.9bffb773.js.map
