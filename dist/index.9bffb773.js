b73327b891c5bc3b185a4f6ffa677bb8f58dbfeb
jujuna10 <jujunaniko@gmail.com>
2024-08-07 22:35:47 +0400

    commit

--- dist/index.9bffb773.js
+++
@@ -1,2 +0,0 @@
-const t=document.getElementById("p"),e=document.getElementById("plus"),n=document.getElementById("minus");e.addEventListener("click",()=>{t.textContent=Number(t.textContent)+1}),n.addEventListener("click",()=>{t.textContent=Number(t.textContent)-1});
-//# sourceMappingURL=index.9bffb773.js.map
